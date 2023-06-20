505804356c02bda4a2c772b6cade836ca23f961a
TranTranHuy <121931706+TranTranHuy@users.noreply.github.com>
2023-06-20 10:16:09 +0700

    Upload

---
+++ connect.php
@@ -0,0 +1,40 @@
+<?php
+
+class connect{
+    public $server;
+    public $user;
+    public $password;
+    public $dbName;
+
+    public function __construct(){
+        $this->server ="localhost";
+        $this->user = "root";
+        $this->password = "";
+        $this->dbName = "test1";
+    }
+
+    function connectToMySQL():mysqli{
+        $conn_my = new mysqli($this->server,$this->user,$this->password,$this->dbName);
+        if($conn_my->connect_error) {
+            die("Fail!".$conn_my->connect_error);
+        }
+        else{
+
+        }
+        return $conn_my;
+    }
+    function connectToPDO():PDO{
+        try{
+            $conn_pdo = new PDO("mysql:host=$this->server;dbname=$this->dbName",$this->user,$this->password);
+            $conn_pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch(PDOException $e)
+        {
+            die("Failed $e");
+        }
+        return $conn_pdo;
+    }
+}
+
+
+?>
